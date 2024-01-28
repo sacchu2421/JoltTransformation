c7058389c0d90121ca2084374bf9c287537cb9a8
sacchu2421 <sachinkapkoti1234@gmail.com>
2024-01-29 00:39:00 +0530

    Comments removed from JoltUtils

--- src/main/java/com/example/integration/exception/ExceptionHandler.java
+++ src/main/java/com/example/integration/exception/ExceptionHandler.java
@@ -10,12 +10,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(RuntimeException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(CustomException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleMyCustomException(CustomException ex) {
         ErrorResponse response = new ErrorResponse(ex.getStatusCode(), ex.getErrorMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
-   //TODO Need to find a way to eliminate ErrorResponse class as there are multiple classes we have as of now
     // Add more exception handlers for other exceptions if needed
 }
